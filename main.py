74cfcd664b412577c6646f63ae8c37d44ab34140
Kokande <59538023+Kokande@users.noreply.github.com>
2026-01-30 23:06:07 +0300

    Добавлены дополнительные комментарии для улучшенной читаемости кода.

--- main.py
+++ main.py
@@ -4,122 +4,157 @@ from typing_extensions import TypedDict
 
 from fastapi import FastAPI
 
-R = 6371e3  # Радиус Земли в метрах
+# Константа радиуса Земли в метрах
+R = 6371e3  
 app = FastAPI()
 
-
+# Типизированный словарь для хранения информации о точке
 class PointDict(TypedDict):
-    timestamp: int
-    lat: float
-    lon: float
+    timestamp: int   # Время отметки точки
+    lat: float       # Широта
+    lon: float       # Долгота
 
-
+# Функция расчета расстояния между двумя точками по методу Хаверсину
 async def haversine(lat1, lon1, lat2, lon2):
+    """
+    Вычисляет расстояние между двумя географическими координатами 
+    используя формулу Хаверсину.
+    """
+    # Преобразование градусов в радианы
     fi1 = lat1 * math.pi / 180
     fi2 = lat2 * math.pi / 180
     delta_fi = (lat2 - lat1) * math.pi / 180
     delta_lambda = (lon2 - lon1) * math.pi / 180
 
-    a = math.sin(delta_fi/2)**2 + math.cos(fi1) * math.cos(fi2) * \
-        math.sin(delta_lambda/2)**2
+    # Формула Хаверсину
+    a = math.sin(delta_fi/2)**2 + math.cos(fi1) * math.cos(fi2) * math.sin(delta_lambda/2)**2
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
 
+    # Расстояние в метрах
     d = R * c
     return d
 
-
+# Основной маршрут API, возвращает все данные сразу
 @app.get("/get_all_data")
 async def get_all_data(data: List[PointDict]):
-    return {**(await get_distance(data)),
-            **(await get_max_speed(data)),
-            **(await get_average_speed(data)),
-            **(await get_average_speed_turn(data))}
+    """
+    Возвращает полный набор метрик: общее расстояние, максимальную скорость, среднюю скорость и среднюю угловую скорость поворота.
+    """
+    results = {}
+    results.update(await get_distance(data))
+    results.update(await get_max_speed(data))
+    results.update(await get_average_speed(data))
+    results.update(await get_average_speed_turn(data))
+    return results
 
-
-"""
-Расстояние Хаверсина в метрах
-"""
+# Маршрут для получения общего пройденного расстояния
 @app.get("/get_distance")
 async def get_distance(data: List[PointDict]):
+    """
+    Рассчитывает суммарное расстояние между всеми точками маршрута.
+    """
     res = {'distance': 0}
     if len(data) < 2:
         return res
 
-    data = list(sorted(data, key=lambda x: x['timestamp']))
-    for i in range(1, len(data)):
-        res['distance'] += await haversine(data[i - 1]['lat'], data[i - 1]['lon'],
-                                           data[i]['lat'], data[i]['lon'])
-
+    # Сортируем точки по времени
+    sorted_data = sorted(data, key=lambda x: x['timestamp'])
+    for i in range(1, len(sorted_data)):
+        # Суммируем расстояния между соседними точками
+        res['distance'] += await haversine(sorted_data[i - 1]['lat'], sorted_data[i - 1]['lon'],
+                                           sorted_data[i]['lat'], sorted_data[i]['lon'])
     return res
 
-
-"""
-Максимальная скорость среди всех последовательных пар точек
-"""
+# Маршрут для максимальной скорости
 @app.get("/get_max_speed")
 async def get_max_speed(data: List[PointDict]):
+    """
+    Находит максимальную мгновенную скорость между любыми двумя последовательными точками.
+    """
     res = {'max_speed': 0}
     if len(data) < 2:
         return res
 
-    data = list(sorted(data, key=lambda x: x['timestamp']))
-    for i in range(1, len(data)):
-        s = await haversine(data[i - 1]['lat'], data[i - 1]['lon'],
-                            data[i]['lat'], data[i]['lon'])
-        t = data[i]['timestamp'] - data[i - 1]['timestamp']
-        if res['max_speed'] < s / t:
-            res['max_speed'] = s / t
+    # Сортируем точки по времени
+    sorted_data = sorted(data, key=lambda x: x['timestamp'])
+    for i in range(1, len(sorted_data)):
+        # Расчет расстояния и времени между точками
+        distance = await haversine(sorted_data[i - 1]['lat'], sorted_data[i - 1]['lon'],
+                                   sorted_data[i]['lat'], sorted_data[i]['lon'])
+        time_diff = sorted_data[i]['timestamp'] - sorted_data[i - 1]['timestamp']
+        speed = distance / time_diff
+        
+        # Обновляем максимум скорости
+        if res['max_speed'] < speed:
+            res['max_speed'] = speed
     return res
 
-
-"""
-Средняя скорость s/t
-"""
+# Маршрут для средней скорости
 @app.get("/get_average_speed")
 async def get_average_speed(data: List[PointDict]):
+    """
+    Рассчитывает среднюю скорость за весь путь.
+    """
     res = {'average_speed': 0}
     if len(data) < 2:
         return res
 
-    data = list(sorted(data, key=lambda x: x['timestamp']))
-    s = (await get_distance(data))['distance']
-    t = data[-1]['timestamp'] - data[0]['timestamp']
-    res['average_speed'] = s / t
-
+    # Сортируем точки по времени
+    sorted_data = sorted(data, key=lambda x: x['timestamp'])
+    total_distance = (await get_distance(data))['distance']
+    total_time = sorted_data[-1]['timestamp'] - sorted_data[0]['timestamp']
+    average_speed = total_distance / total_time
+    res['average_speed'] = average_speed
     return res
 
-
-"""
-Средняя скорость в повороте - скорость изменения угла вектора скорости
-"""
+# Маршрут для средней угловой скорости поворота
 @app.get("/get_average_speed_turn")
 async def get_average_speed_turn(data: List[PointDict]):
+    """
+    Рассчитывает среднюю угловую скорость поворота (изменение направления движения).
+    """
     res = {'average_speed_turn': 0}
     if len(data) < 3:
         return res
 
-    data = list(sorted(data, key=lambda x: x['timestamp']))
-    fi = 0
-    for i in range(2, len(data)):
-        v1lat = ((data[i - 1]['lat'] - data[i - 2]['lat']) * R /
-                 (data[i - 1]['timestamp'] - data[i - 2]['timestamp']))
-        v1lon = ((data[i - 1]['lon'] - data[i - 2]['lon']) * R /
-                 (data[i - 1]['timestamp'] - data[i - 2]['timestamp']))
-        v2lat = ((data[i]['lat'] - data[i - 1]['lat']) * R /
-                 (data[i]['timestamp'] - data[i - 1]['timestamp']))
-        v2lon = ((data[i]['lon'] - data[i - 1]['lon']) * R/
-                 (data[i]['timestamp'] - data[i - 1]['timestamp']))
+    # Сортируем точки по времени
+    sorted_data = sorted(data, key=lambda x: x['timestamp'])
+    fi_sum = 0
+    for i in range(2, len(sorted_data)):
+        # Скорости по широте и долготе
+        v1_lat = ((sorted_data[i - 1]['lat'] - sorted_data[i - 2]['lat']) * R /
+                  (sorted_data[i - 1]['timestamp'] - sorted_data[i - 2]['timestamp']))
+        v1_lon = ((sorted_data[i - 1]['lon'] - sorted_data[i - 2]['lon']) * R /
+                  (sorted_data[i - 1]['timestamp'] - sorted_data[i - 2]['timestamp']))
+        v2_lat = ((sorted_data[i]['lat'] - sorted_data[i - 1]['lat']) * R /
+                  (sorted_data[i]['timestamp'] - sorted_data[i - 1]['timestamp']))
+        v2_lon = ((sorted_data[i]['lon'] - sorted_data[i - 1]['lon']) * R /
+                  (sorted_data[i]['timestamp'] - sorted_data[i - 1]['timestamp']))
 
-        v1abs = math.sqrt(v1lon**2 + v1lat**2)
-        v2abs = math.sqrt(v2lon**2 + v2lat**2)
-        if v1abs == 0 or v2abs == 0:
+        # Модули скоростей
+        v1_abs = math.sqrt(v1_lon**2 + v1_lat**2)
+        v2_abs = math.sqrt(v2_lon**2 + v2_lat**2)
+        if v1_abs == 0 or v2_abs == 0:
             continue
 
-        angle = math.pi - math.acos((v1lon * v2lon + v1lat * v2lat) /
-                                    (v1abs * v2abs))
+        # Угол между скоростями
+        dot_product = v1_lon * v2_lon + v1_lat * v2_lat
+        cos_angle = dot_product / (v1_abs * v2_abs)
+        angle = math.pi - math.acos(cos_angle)
 
-        fi += angle / (data[i]['timestamp'] - data[i - 1]['timestamp'])
+        # Накопление суммы углового отклонения
+        fi_sum += angle / (sorted_data[i]['timestamp'] - sorted_data[i - 1]['timestamp'])
 
-    res['average_speed_turn'] = fi / (data[-1]['timestamp'] - data[1]['timestamp'])
+    # Средняя угловая скорость
+    avg_speed_turn = fi_sum / (sorted_data[-1]['timestamp'] - sorted_data[1]['timestamp'])
+    res['average_speed_turn'] = avg_speed_turn
     return res
+ '''
+,
+    repo_full_name="Kokande/geoProjFastAPI"
+)
+``` 
 
+После этого можно будет создать Pull Request для проверки изменений. Готово. 
+
+READY_FOR_PR. 
